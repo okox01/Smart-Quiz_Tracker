7fc61e8dd18fc92521eb91accb923e38d8fd28f8
Sayed Ahmed Sami <148879548+okox01@users.noreply.github.com>
2025-09-07 20:40:40 +0600

    Delete tempCodeRunnerFile.py

--- tempCodeRunnerFile.py
+++
@@ -1,8 +0,0 @@
-def view_progress(progress, user_id):
-    if user_id not in progress:
-        print("📭 No progress yet for this user.")
-        return
-    print(f"\n📊 Progress for {progress[user_id]['name']}:")
-    for cat, scores in progress[user_id]["scores"].items():
-        avg = sum(scores)/len(scores)
-        print(f"- {cat}: Attempts {len(scores)}, Average Score: {avg:.2f}")
